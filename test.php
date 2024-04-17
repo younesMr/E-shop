1d37a88d5b512e473fb8549c586a918cc0f69103
Oussamabhs <167133728+Oussamabhs@users.noreply.github.com>
2024-04-17 14:19:02 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<?PHP
+$servername="localhost";
+$DBuser= "roots";
+$DBpassword="";
+$DBname= "e-commerce";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$DBname", $DBuser,$DBpassword);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+
+
+?>
